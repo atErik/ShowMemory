954d4ab8dd4982ce037ba89d6e80d81e00a7ba7b
atErik <aterik@outlook.com>
2020-06-28 21:23:23 -0700

    v0.59

--- ShowMemory.sh
+++ ShowMemory.sh
@@ -2,7 +2,7 @@
 #
 # *************************************************************************
 # *************************************************************************
-# * ShowMemory.sh - ShowMemory - shows Free RAM, Used RAM, Total RAM, etc.
+# * ShowMemory.sh - ShowMemory - it shows Free/Used/Total/etc RAM/Memory Size/Amount.
 # * Copyright © 2020 Erik T Ashfolk (<atErïk＠ÖυťĹöōķ·ċōm;atErïk＠AśhFölķ·ćōm>, Do Not Copy 
 # *  Eml-Adrs, Type in English/basic-latin, No Soliciting Permitted). All rights reserved.
 # * Written initially on 2020-06-24 by Erik T Ashfolk.
@@ -10,39 +10,52 @@
 # *  (*) Do Not Use My/Our Contribution(s) To Kill/Harm/Violate(or Steal-from)(Any) Human/Community,Earth,etc.
 # *  (*) GNU General Public License v3 (GPL v3) https://www.GNU.org/licenses/gpl-3.0.en.html 
 # *
-# * ShowMemory.sh v0.58
+# * ShowMemory.sh v0.59
 # * It shows Free RAM, Used RAM, Total RAM, etc memory size/amount.
 # *************************************************************************
 # *************************************************************************
 #
-# A sample/example Output of "vm_stat" command, on macOSX:
-# 01 |Mach Virtual Memory Statistics: (page size of 4096 bytes)
-# 02 |Pages free:                               29349.
-# 03 |Pages active:                            687993.
-# 04 |Pages inactive:                          682768.
-# 05 |Pages speculative:                         4862.
-# 06 |Pages throttled:                              0.
-# 07 |Pages wired down:                        360086.
-# 08 |Pages purgeable:                           3178.
-# 09 |"Translation faults":                 121433300.
-# 10 |Pages copy-on-write:                    2739034.
-# 11 |Pages zero filled:                     90069451.
-# 12 |Pages reactivated:                      2688913.
-# 13 |Pages purged:                            472357.
-# 14 |File-backed pages:                       317426.
-# 15 |Anonymous pages:                        1058197.
-# 16 |Pages stored in compressor:              899695.
-# 17 |Pages occupied by compressor:            331547.
-# 18 |Decompressions:                         1301864.
-# 19 |Compressions:                           2410285.
-# 20 |Pageins:                                9984186.
-# 21 |Pageouts:                                 86461.
-# 22 |Swapins:                                      0.
-# 23 |Swapouts:                                   192.
+# Line# | A sample/example Output of "vm_stat" command, on macOSX:
+# 01    | Mach Virtual Memory Statistics: (page size of 4096 bytes)
+# 02    | Pages free:                               29349.
+# 03    | Pages active:                            687993.
+# 04    | Pages inactive:                          682768.
+# 05    | Pages speculative:                         4862.
+# 06    | Pages throttled:                              0.
+# 07    | Pages wired down:                        360086.
+# 08    | Pages purgeable:                           3178.
+# 09    | "Translation faults":                 121433300.
+# 10    | Pages copy-on-write:                    2739034.
+# 11    | Pages zero filled:                     90069451.
+# 12    | Pages reactivated:                      2688913.
+# 13    | Pages purged:                            472357.
+# 14    | File-backed pages:                       317426.
+# 15    | Anonymous pages:                        1058197.
+# 16    | Pages stored in compressor:              899695.
+# 17    | Pages occupied by compressor:            331547.
+# 18    | Decompressions:                         1301864.
+# 19    | Compressions:                           2410285.
+# 20    | Pageins:                                9984186.
+# 21    | Pageouts:                                 86461.
+# 22    | Swapins:                                      0.
+# 23    | Swapouts:                                   192.
 
 
 LnNumbr=1;
+
 totalPhysicalMem=0;
+pgFree=0;
+pgActv=0;
+pgInactv=0;
+pgSpecu=0;
+pgThrot=0;
+pgWirdDn=0;
+pgPurgbl=0;
+pgFileBkd=0;
+pgCmprsStor=0;
+pgCmprsr=0;
+
+# Flag Variables for above content varibales:
 pgSizeRcvd=0;
 pgFreeRcvd=0;
 pgActvRcvd=0;
@@ -56,23 +69,33 @@ pgCmprsStorRcvd=0;
 pgCmprsrRcvd=0;
 
 while read -u3 -r Ln ; do
-	memInLn="${Ln//\ /}";  memInLn="${memInLn#*\:}";  memInLn="${memInLn//\./}";
+	# Removing all 'space'-chars ; Removing 1st ":" colon in left-side, & anything in it's left side:
+	memInLn="${Ln//\ /}";  memInLn="${memInLn#*\:}";
+	# Removing all '.' dot-chars:
+	memInLn="${memInLn//\./}";
+	# It contains/shows Number of "Pages"-memory-unit, Multiplying With pgSize bytes = Total Bytes,
+	# then Dividing with 1MBytes(1048576) to get size in MB: 
 	memInLn=$(( ( $memInLn * pgSize ) / 1048576 ));		
-	
+	# Collecting "Page" count/number from a specific line (only one time), when that line is read in this loop:
 	if [ "$pgSizeRcvd" -eq "0" ] && [[ "$Ln" == *"bytes"* ]] ; then
 		# removing anything before "of " & this word itself ; removing all after " bytes" & this word itself ;
 		pgSize="${Ln#*of\ }";  pgSize="${pgSize%\ bytes*}";
+		# As we have read this specific line, this if-condition-then-fi section needs to be skipped in next loops:
 		pgSizeRcvd=1;
-		# printf "\174 $pgSize \174\n";
+		# printf "\174 $pgSize \174\n";  # debug
+		# Only for this top/1st line, the memInLn needs ot be "MB" to show the "MB" in the header/top row line:
 		memInLn="MB";
-		Ln="$Ln (KB)";
+	else
+		# When a line is not the top-most header-row line (with the text "(page size of 4096 bytes)")
+		#  then we add 9-extra-space-chars at 32nd-position, to align it with top/header-row:
+		Ln="${Ln:0:32}         ${Ln:32}";
 	fi;
 	if [ "$pgFreeRcvd" -eq "0" ] && [[ "$Ln" == *"Pages free"* ]] ; then
-		# removing anything before colon & the colon ; removing all spaces ; removing all dots ;
+		# Removing 1st colon in left-side, & the colon itself ; Removing all spaces ; Removing all dots ;
 		pgFree="${Ln#*\:}";  pgFree="${pgFree//\ /}";  pgFree="${pgFree//\./}";
 		pgFreeRcvd=1;
 		totalPhysicalMem=$(( $totalPhysicalMem + $pgFree ));
-		# printf "\174 $pgFree \174\n";
+		# printf "\174 $pgFree \174\n";  # debug
 	fi;
 	if [ "$pgActvRcvd" -eq "0" ] && [[ "$Ln" == *"Pages active"* ]] ; then
 		pgActv="${Ln#*\:}";  pgActv="${pgActv//\ /}";  pgActv="${pgActv//\./}";
@@ -116,17 +139,19 @@ while read -u3 -r Ln ; do
 		pgCmprsrRcvd=1;
 		totalPhysicalMem=$(( $totalPhysicalMem + $pgCmprsr ));
 	fi;
-
+	
+	# showing each line from "vm_stat" command on screen plus few more data 
 	if [ "$LnNumbr" -le "9" ] ; then
 		m="0$LnNumbr";
-		# the "\174" is pipe-symbol's value in Octal number.
-		printf "\174$m\174 $Ln \174 $memInLn \174\n";
+		# the "\174" is "|" pipe-symbol's RefNum value in Octal number-system. \011 is Tab 
+		printf "\174$m\174 $Ln \174 $memInLn\011 \174\n";
 	else
-		printf "\174$LnNumbr\174 $Ln \174 $memInLn \174\n";
+		printf "\174$LnNumbr\174 $Ln \174 $memInLn\011 \174\n";
 	fi;
 	LnNumbr=$(( $LnNumbr + 1 ));
 done 3< <(/usr/bin/vm_stat);
 # We used "vm_stat", as it appears that, it does not need privileged access to run
+
 
 # 1 MegaBytes(MB) = 1048576 Bytes
 echo "CATEGORY OF MEMORY              : MB (MegaBytes)";
@@ -137,7 +162,7 @@ echo "Used (Physical/RAM) Memory      : $(( (( $pgActv + $pgWirdDn + $pgCmprsr )
 echo "Total (Physical/RAM) Memory     : $(( ( $totalPhysicalMem * $pgSize ) / 1048576 ))";
 
 
-# Below bash command-set will output actual RAM mem size, but this command may need privileged access to run:
+# Below bash command-set will output actual RAM mem size, but below command may need privileged access to run:
 # ramBytes="`sysctl hw.memsize`"; echo "$ramBytes" ; ramBytes="${ramBytes#*\:\ }"; echo "$ramBytes Bytes"; ramMB=$(( $ramBytes / 1048576 )); echo "$ramMB MB"; ramGB=$(( $ramBytes / 1073741824 )); echo "$ramGB GB"; unset ramBytes ramMB ramGB;
 
 unset m memInLn;
